65180455860e1e89620491eb3c4a5ece64cc4a18
Ednaldo Júnior <ednaldo.junior@dce.ufpb.br>
2014-09-08 13:13:26 -0300

    testando

---
+++ src/main/java/br/ufcg/ppgcc/compor/ir/impl/ImpostoDeRenda.java
@@ -0,0 +1,20 @@
+package br.ufcg.ppgcc.compor.ir.impl;
+import java.util.List;
+
+import br.ufcg.ppgcc.compor.ir.FachadaExperimento;
+import br.ufcg.ppgcc.compor.ir.Titular;
+
+public class ImpostoDeRenda implements FachadaExperimento {
+
+	public void criarNovoTitular(Titular titular) {
+		
+		
+	}
+
+	public List<Titular> listarTitulares() {
+		
+		List<Titular> titular = null;
+		return titular;
+	}
+
+}
