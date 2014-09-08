d723acdf068dd58aa21172b023dfb7e512b6597f
Ednaldo Júnior <ednaldo.junior@dce.ufpb.br>
2014-09-08 13:41:00 -0300

    Tarefa T.01.01 finalizada

--- src/main/java/br/ufcg/ppgcc/compor/ir/impl/ImpostoDeRenda.java
+++ src/main/java/br/ufcg/ppgcc/compor/ir/impl/ImpostoDeRenda.java
@@ -1,20 +1,22 @@
 package br.ufcg.ppgcc.compor.ir.impl;
+import java.util.ArrayList;
 import java.util.List;
 
 import br.ufcg.ppgcc.compor.ir.FachadaExperimento;
 import br.ufcg.ppgcc.compor.ir.Titular;
 
-public class ImpostoDeRenda implements FachadaExperimento {
-
+public class ImpostoDeRenda implements FachadaExperimento{
+	
+	List<Titular> titulares = new ArrayList<Titular>();
 	public void criarNovoTitular(Titular titular) {
 		
-		
+		titulares.add(titular);	
 	}
 
 	public List<Titular> listarTitulares() {
 		
-		List<Titular> titular = null;
-		return titular;
+		return titulares;
 	}
-
+	
 }
+
