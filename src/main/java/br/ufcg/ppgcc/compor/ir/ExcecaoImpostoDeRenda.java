5cebc2fb9ef70e18664225136a887eb73a56d363
Ednaldo Júnior <ednaldo.junior@dce.ufpb.br>
2014-09-10 23:46:31 -0300

    ok

--- src/main/java/br/ufcg/ppgcc/compor/ir/ExcecaoImpostoDeRenda.java
+++
@@ -1,9 +0,0 @@
-package br.ufcg.ppgcc.compor.ir;
-
-@SuppressWarnings("serial")
-public class ExcecaoImpostoDeRenda extends RuntimeException {
-
-	public ExcecaoImpostoDeRenda(String msg) {
-		super(msg);
-	}
-}
